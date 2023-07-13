f283e4b94da1e59bcb27834991b698b3097e9c6a
johnkagiri <kagirijohn124@gmail.com>
2023-07-13 23:38:39 +0300

    Completed asssignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution here
+var drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(array,condition){
+    return array.filter(function (el){
+       return el.toLowerCase().includes(condition.toLowerCase());
+    })
+}
+console.log(findMatching(drivers,'bby'));
+function fuzzyMatch(array,condition){
+  return array.filter(function (el){
+    return el.startsWith(condition);
+ })
+}
+console.log(findMatching(drivers,'Bo'))
+{
+    const drivers = [
+        {
+          name: 'Bobby',
+          hometown: 'Pittsburgh' },
+        {
+          name: 'Sammy',
+          hometown: 'New York' } ,
+        {
+          name: 'Sally',
+          hometown: 'Cleveland' },
+        {
+          name: 'Annette',
+          hometown: 'Los Angeles' },
+        {
+          name: 'Bobby',
+          hometown: 'Tampa Bay' }
+      ];
+function matchName(array, condition){
+    return array.filter(function (el){
+        return el['name'].toLowerCase().includes(condition.toLowerCase());
+     })
+}
+console.log(matchName(drivers,'Bo'));
+}
